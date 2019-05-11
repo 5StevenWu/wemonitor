265414c9f8fcfcf1bc21a938778ea5b2d1f24bfd
hsingpu <1927833039@qq.com>
2019-05-11 13:05:57 +0800

    修改readconf路径问题  及 添加注释

--- libs/read_config.py
+++ libs/read_config.py
@@ -3,14 +3,15 @@
 import os
 import json
 
-homedir = os.path.dirname(os.path.dirname(__file__))  # D:\code\zenthought\wemonitor  #此种写法可以在任意非当前路径调用
-conf_file = 'conf/wechatConf.zqconfig'
-realdir = os.path.join(homedir, conf_file)  # D:/code/zenthought/wemonitor\conf/wechatConf.zqconfig
-print(realdir)
+
+# homedir = os.path.dirname(os.path.dirname(__file__))  # D:\code\zenthought\wemonitor  #此种写法可以在任意非当前路径调用
+# conf_file = 'conf/wechatConf.zqconfig'
+# realdir = os.path.join(homedir, conf_file)  # D:/code/zenthought/wemonitor\conf/wechatConf.zqconfig
+# print(realdir)
 
 
 def loadConf():
-    with open(realdir, 'rU') as wechatConf:
+    with open('../conf/wechatConf.zqconfig', 'rU') as wechatConf:
         settings = json.load(wechatConf)
         corpid = settings['corpid']
         corpsecret = settings['corpsecret']
