c8b28d6c86c9631a55eccaba9771e954de756f81
hsingpu <1927833039@qq.com>
2019-07-25 17:08:23 +0800

    '保留老版本更新新api'

--- sendwechat/views.py
+++ sendwechat/views.py
@@ -4,6 +4,7 @@ from sendwechat.send2wechat import send_msg
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
 
 class sendWechat(APIView):
@@ -18,20 +19,14 @@ class sendWechat(APIView):
         return HttpResponse('欢迎使用报警平台')
 
 
-class sendWechat(APIView):
+# http://127.0.0.1:8000/sendwechat/?title='报警标题'&content='报警内容'
+
+class wxmonitor(APIView):
 
     def get(self, request):
-        # title = request.GET.get('title')
-        # content = request.GET.get('content')
-        # if title:
-        #     print(title, content)
-        #     ret = send_msg(title, content)
-        #     return HttpResponse(str(ret))
-        # return HttpResponse('OK')
+        return render(request, 'wechat.html')
 
-        return render(request,'wechat.html')
-
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         title = request.POST.get('title')
         content = request.POST.get('content')
         if title:
@@ -40,6 +35,3 @@ class sendWechat(APIView):
             print(type(res))
             return Response(res)
         return HttpResponse('测试失败')
-
-
-# http://127.0.0.1:8000/sendwechat/?title='报警标题'&content='报警内容'
--- wemonitor/urls.py
+++ wemonitor/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     # url(r'^index/', views.sendWechat.as_view()),
     # url(r'^sendall/', views.sendWechat.as_view()),
     url(r'^sendwechat/', views.sendWechat.as_view()),
+    url(r'^api/v3/wxmonitor/', views.wxmonitor.as_view()),
     # url(r'^sendmail/', views.sendWechat.as_view()),
     # url(r'^sendsms/', views.sendWechat.as_view()),
 ]
