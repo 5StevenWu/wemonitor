3ec7c733689e00cf30f0a5e8d0ada42641c8a470
hsingpu <1927833039@qq.com>
2022-03-21 14:27:34 +0800

    关闭F12模式  修改为新版selenium api

--- scriptTest/bin/chrome_dev_monitor.py
+++ scriptTest/bin/chrome_dev_monitor.py
@@ -8,39 +8,58 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from PIL import ImageGrab
 from send2alimail import sendmail
+from selenium.webdriver.common.by import By
+import random
 
-#============================打开浏览器开发者模式===========================
+# ============================打开浏览器开发者模式===========================
 options = webdriver.ChromeOptions()
-options.add_argument("--auto-open-devtools-for-tabs")
+# options.add_argument("--auto-open-devtools-for-tabs") #打开调试页面
 # options.add_experimental_option('excludeSwitches', ['enable-automation'])
 
 
-driver = webdriver.Chrome(executable_path=r'..\libs\chromedriver.exe',chrome_options=options)
+browser = webdriver.Chrome(executable_path=r'..\libs\chromedriver.exe', chrome_options=options)
 # driver = webdriver.Chrome(executable_path=r'F:\全栈21期\day104爬虫\chromedriver_win3276\chromedriver.exe',)
-driver.get('http://www.qq.com')
-sleep(1)
+browser.get('http://www.baidu.com')
+# builder = ActionChains(driver)
+sleep(2)
+# ele_input=driver.find_elements(By.XPATH,'//*[@id="kw"]').send_keys('seleniumhq' + Keys.RETURN)
+
+browser.find_element(By.ID, 'kw').send_keys('天翼云' + Keys.RETURN)
+
+#browser.find_element(By.ID, 'kw').send_keys('seleniumhq' + Keys.RETURN)
+
+sleep(random.randrange(2,6,1))
+
+
+browser.find_element(By.XPATH,'//*[@id="page"]/div/a[10]').click()
+sleep(3)
+
+res=browser.find_element(By.XPATH,'//*[@id="wz864u"]/div/span').text
+print(res)
+
 
 # driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL+Keys.SHIFT+'j')
-builder = ActionChains(driver)
+
 # builder.send_keys('test')
-# builder.key_down(Keys.CONTROL).send_keys('a')
+# builder.key_down(Keys.ENTER).send_keys('TEST')
+
+
 # builderx = ActionChains(driver)
 # builder.key_down(Keys.CONTROL).send_keys(']')
 # builder.key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('j').key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
 # builder.key_down(Keys.F12).perform()
 # builder.key_up(Keys.F12).perform()
 # builder.key_down(Keys.CONTROL+']').perform()
-builder.key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('j').perform()
+# builder.key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('j').perform()  #F12
 def run():
     while True:
         sleep(5)
         # builder.key_down(Keys.F5).perform()
         driver.refresh()
         # driver.save_screenshot('shortcut.png')
-        #=========================截图========================
-        im = ImageGrab.grab()
-        im.save(r'./shortcut.png')
-
+        # =========================截图========================
+        # im = ImageGrab.grab()
+        # im.save(r'./shortcut.png')
 
         # ===============发送邮件接口===========================
         # sendmail()
